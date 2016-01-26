087c27cac9979d7e084007e4cfc8667b8e9b22b3
Keiro <admin@heimkoma.com>
2016-01-26 14:34:39 -0600

    Clean up RaspbianPackages.sh
    Clean up code. Make it look prettyfuls.

--- RaspbianPackages.sh
+++ RaspbianPackages.sh
@@ -4,8 +4,9 @@
 # Version 0.1.
 
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install dnsutils fail2ban iotop htop iftop nethogs pv lzop xz-utils python python-dev libjpeg-dev libfreetype6-dev python-setuptools python-pip x11-apps x11-xserver-utils xterm pyroom git conky-std alsa-utils mpg321 lame fonts-inconsolata screen screenie build-essential bluez dos2unix libgnutls28-dev telnet
+sudo apt-get install dnsutils fail2ban samba iotop htop iftop ntop nethogs pv lzop xz-utils python python-dev libjpeg-dev libfreetype6-dev python-setuptools python-pip x11-apps x11-xserver-utils xterm vim vim-gtk pyroom git conky-std alsa-utils mpg321 lame fonts-inconsolata screen screenie build-essential bluez dos2unix libgnutls28-dev telnet
 sudo pip install --upgrade pip
+
 
 # Chromium:
 
@@ -26,6 +27,9 @@ cd ~ || exit
 wget https://download.configserver.com/csf.tgz
 tar -xzf csf.tgz
 cd csf  || exit
+# install.sh is for cPanel-only servers. NEVER install it on non-cPanel servers.
+# Instead, run install.generic.sh, which is designed to run in either a cli
+# environment or a Webmin-enabled environment.
 sudo sh install.generic.sh
 sudo perl /usr/local/csf/bin/csftest.pl
 
@@ -39,19 +43,25 @@ service apache2 stop
 # Note, this command is meant to be used on first run. Subsequent runs will not require everything.
 sudo ./letsencrypt-auto --agree-tos --renew-by-default --text --email=admin@heimkoma.com -d raspie.heimkoma.com certonly
 service apache2 start
+
+
 # The rest of this code is commented out to prevent RaspbianPackages.sh from renewing the SSL certificate
 # prematurely. This is the minimum necessary to renew SSL certificates after the initial run.
 # Further, you can actually automate this process via crontab!
 # Subsequent runs will be something similar to the following:
+
 #sudo ./letsencrypt-auto -c /root/cli.ini certonly
 #service apache2 start
 
 # Code for crontab automation for LE cert renewals:
 # Include in this in something like renew.sh
-# crontab -e
-# 00 00  *  *   * every 60 days && echo test
+
+#crontab -e
+#00 00  *  *   * every 60 days && echo test
+
 # Cribbed from: http://stackoverflow.com/questions/4549542/cron-job-every-three-days answer from Vishal.
 # Adjust code here to your needs.
+
 #!/bin/sh
 #case $2 in
 #    days)
@@ -66,9 +76,9 @@ service apache2 start
 #        expr `date +%m` % $1 = 0 > /dev/null
 #        ;;
 #esac
+
 #!/bin/bash
 # Get new keys!
 #/root/letsencrypt/letsencrypt-auto -c /root/cli.ini certonly
-
 # Reload Services
 #service apache2 restart
